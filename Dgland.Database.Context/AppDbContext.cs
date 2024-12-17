9f09f03f616658fbd935416b1960aff9d2c0d5a2
Maahyar Azad <mhyrinc@gmail.com>
2024-12-17 12:06:41 +0400

    Add project files.

---
+++ Dgland.Database.Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Dgland.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dgland.Database.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BaseEntity).Assembly);
+        }
+    }
+}
---
+++ Dgland.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Dgland.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
